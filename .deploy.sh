fa3849682c16a34feef9d1e3753415daf723f2b0
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2018-01-06 13:48:40 +0900

    update

--- .deploy.sh
+++ .deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-set -x
+# set -x
 
 cd $CI_SOURCE_PATH
 
-WWWDIR=deploy
+WWWDIR=public
 if [ -d $WWWDIR ]; then
   rm -rf $WWWDIR
 fi
@@ -21,12 +21,12 @@ catkin_topological_order | while read line; do
   fi
 done
 
-git config --global user.name "Travis-CI"
-git config --global user.email "dummy@example.com"
+# git config --global user.name "Travis-CI"
+# git config --global user.email "dummy@example.com"
 
-cd $WWWDIR
-git init
-git add . --all
-git commit -m "Deploy to GitHub Pages"
-# Don't remove '> /dev/null 2>&1'. Removing this will expose GH_TOKEN
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:master > /dev/null 2>&1
+# cd $WWWDIR
+# git init
+# git add . --all
+# git commit -m "Deploy to GitHub Pages"
+# # Don't remove '> /dev/null 2>&1'. Removing this will expose GH_TOKEN
+# git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:master > /dev/null 2>&1
